70b0764120b332fb5f8e93c7c43d7b8b328bab46
davorradic <44604843+davorradic@users.noreply.github.com>
2018-11-11 16:26:51 +0100

    first commit contact page

---
+++ contact.php
@@ -0,0 +1,7 @@
+<?php include("template/header.php") ?>
+  <form class="" action="index.html" method="post">
+    <input type="text" value="name" />
+    <button>submit</submit>
+
+  </form>
+<?php include("template/footer.php") ?>
